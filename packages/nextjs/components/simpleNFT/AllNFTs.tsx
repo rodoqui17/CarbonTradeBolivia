7c0c92513be874a440766fe6448405e04b64c9fd
Rodrigo Quiroz <quiroz.rodrigo@gmail.com>
2023-09-27 20:56:11 -0400

    Update AllNFTs.tsx
    actualiza todos los usuarios

--- packages/nextjs/components/simpleNFT/AllNFTs.tsx
+++ packages/nextjs/components/simpleNFT/AllNFTs.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { Spinner } from "../Spinner";
 import { NFTCard } from "./NFTCard";
-import { useAccount } from "wagmi";
 import { useScaffoldContract, useScaffoldContractRead } from "~~/hooks/scaffold-eth";
 import { notification } from "~~/utils/scaffold-eth";
 import { NFTMetaData, getNFTMetadataFromIPFS } from "~~/utils/simpleNFT";
@@ -12,37 +11,28 @@ export interface Collectible extends Partial<NFTMetaData> {
   owner: string;
 }
 
-export const MyHoldings = () => {
-  const { address: connectedAddress } = useAccount();
-  const [myAllCollectibles, setMyAllCollectibles] = useState<Collectible[]>([]);
+export const AllNFTs = () => {
+  const [allCollectibles, setAllCollectibles] = useState<Collectible[]>([]);
   const [allCollectiblesLoading, setAllCollectiblesLoading] = useState(false);
 
   const { data: yourCollectibleContract } = useScaffoldContract({
     contractName: "YourCollectible",
   });
 
-  const { data: myTotalBalance } = useScaffoldContractRead({
-    contractName: "YourCollectible",
-    functionName: "balanceOf",
-    args: [connectedAddress],
-    watch: true,
-    cacheOnBlock: true,
-  });
-
   useEffect(() => {
-    const updateMyCollectibles = async (): Promise<void> => {
-      if (myTotalBalance === undefined || yourCollectibleContract === undefined || connectedAddress === undefined)
+    const fetchAllCollectibles = async (): Promise<void> => {
+      if (yourCollectibleContract === undefined)
         return;
 
       setAllCollectiblesLoading(true);
-      const collectibleUpdate: Collectible[] = [];
-      const totalBalance = parseInt(myTotalBalance.toString());
-      for (let tokenIndex = 0; tokenIndex < totalBalance; tokenIndex++) {
+      const collectibles: Collectible[] = [];
+
+      // Puedes modificar este rango según tus necesidades o utilizar una función de lectura para obtener el número total de tokens disponibles.
+      const totalTokensToFetch = 100;
+
+      for (let tokenIndex = 0; tokenIndex < totalTokensToFetch; tokenIndex++) {
         try {
-          const tokenId = await yourCollectibleContract.read.tokenOfOwnerByIndex([
-            connectedAddress,
-            BigInt(tokenIndex.toString()),
-          ]);
+          const tokenId = await yourCollectibleContract.read.tokenByIndex([BigInt(tokenIndex.toString())]);
 
           const tokenURI = await yourCollectibleContract.read.tokenURI([tokenId]);
 
@@ -50,25 +40,27 @@ export const MyHoldings = () => {
 
           const nftMetadata: NFTMetaData = await getNFTMetadataFromIPFS(ipfsHash);
 
-          collectibleUpdate.push({
+          // También puedes obtener la dirección del propietario del NFT si lo necesitas.
+          const owner = await yourCollectibleContract.read.ownerOf([tokenId]);
+
+          collectibles.push({
             id: parseInt(tokenId.toString()),
             uri: tokenURI,
-            owner: connectedAddress,
+            owner: owner,
             ...nftMetadata,
           });
         } catch (e) {
-          notification.error("Error fetching all collectibles");
-          setAllCollectiblesLoading(false);
+          notification.error("Error fetching collectibles");
           console.log(e);
         }
       }
-      setMyAllCollectibles(collectibleUpdate);
+      setAllCollectibles(collectibles);
       setAllCollectiblesLoading(false);
     };
 
-    updateMyCollectibles();
+    fetchAllCollectibles();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [connectedAddress, myTotalBalance]);
+  }, []);
 
   if (allCollectiblesLoading)
     return (
@@ -79,13 +71,13 @@ export const MyHoldings = () => {
 
   return (
     <>
-      {myAllCollectibles.length === 0 ? (
+      {allCollectibles.length === 0 ? (
         <div className="flex justify-center items-center mt-10">
-          <div className="text-2xl text-primary-content">No tienes certificados</div>
+          <div className="text-2xl text-primary-content">No hay NFTs disponibles</div>
         </div>
       ) : (
         <div className="flex flex-wrap gap-4 my-8 px-5 justify-center">
-          {myAllCollectibles.map(item => (
+          {allCollectibles.map(item => (
             <NFTCard nft={item} key={item.id} />
           ))}
         </div>
